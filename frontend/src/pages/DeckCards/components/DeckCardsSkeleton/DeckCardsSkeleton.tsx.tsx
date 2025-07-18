1c938d494b548dd620cc2fff79692adebd8daec4
anhsayshello <anhle0347@gmail.com>
2025-07-18 16:42:47 +0700

    fix skeleton

--- frontend/src/pages/DeckCards/components/DeckCardsSkeleton/DeckCardsSkeleton.tsx.tsx
+++ frontend/src/pages/DeckCards/components/DeckCardsSkeleton/DeckCardsSkeleton.tsx.tsx
@@ -22,67 +22,61 @@ const CardListSkeleton = () => (
   </Card>
 );
 
-export default function DeckCardsSkeleton({
-  isPending,
-}: {
-  isPending?: boolean;
-}) {
+export default function DeckCardsSkeleton() {
   return (
     <div>
-      {isPending && (
-        <div className="md:min-w-[450px]">
-          <div className="flex items-center gap-4">
-            <div className="p-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.2}
-                stroke="currentColor"
-                className="size-4.5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 19.5 8.25 12l7.5-7.5"
-                />
-              </svg>
-            </div>
-            <div className="flex grow h-9 items-center gap-2 border-b px-3">
-              <SearchIcon className="size-4 shrink-0 opacity-50" />
-              <Input
-                type="text"
-                placeholder="Enter a word to search"
-                className="!border-0 !outline-0 !ring-0 !shadow-none focus:!border-0 focus:!outline-none focus:!ring-0 focus-visible:!border-0 focus-visible:!ring-0 aria-invalid:!border-0"
-                disabled
+      <div className="md:min-w-[450px]">
+        <div className="flex items-center gap-4">
+          <div className="p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.2}
+              stroke="currentColor"
+              className="size-4.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
               />
-            </div>
+            </svg>
           </div>
-          <div className="flex items-center justify-between mt-5 mb-4.5">
-            <div className="flex items-center gap-3 flex-wrap">
-              <Button variant="outline" size="sm" disabled>
-                <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
-                Refresh
-              </Button>
-              <Button variant="outline" size="sm" disabled>
-                <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
-                Start practicing
-              </Button>
-              <Button size="sm" variant="outline" disabled>
-                <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
-                Add New Card
-              </Button>
-            </div>
-          </div>
-
-          {/* Loading skeleton grid */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <CardListSkeleton key={index} />
-            ))}
+          <div className="flex grow h-9 items-center gap-2 border-b px-3">
+            <SearchIcon className="size-4 shrink-0 opacity-50" />
+            <Input
+              type="text"
+              placeholder="Enter a word to search"
+              className="!border-0 !outline-0 !ring-0 !shadow-none focus:!border-0 focus:!outline-none focus:!ring-0 focus-visible:!border-0 focus-visible:!ring-0 aria-invalid:!border-0"
+              disabled
+            />
           </div>
         </div>
-      )}
+        <div className="flex items-center justify-between mt-5 mb-4.5">
+          <div className="flex items-center gap-3 flex-wrap">
+            <Button variant="outline" size="sm" disabled>
+              <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
+              Refresh
+            </Button>
+            <Button variant="outline" size="sm" disabled>
+              <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
+              Start practicing
+            </Button>
+            <Button size="sm" variant="outline" disabled>
+              <Skeleton className="w-3.5 h-3.5 rounded animate-pulse"></Skeleton>
+              Add New Card
+            </Button>
+          </div>
+        </div>
+
+        {/* Loading skeleton grid */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <CardListSkeleton key={index} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
