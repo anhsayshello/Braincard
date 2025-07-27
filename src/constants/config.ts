1eea26e721591753581a3e728c279a73e0b082c5
anhsayshello <anhle0347@gmail.com>
2025-07-27 14:03:27 +0700

    update baseurl api

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  baseUrl: "https://braincard-api.vercel.app/",
+  // baseUrl: "https://braincard-api.vercel.app/",
+  baseUrl: "https://braincard-api.up.railway.app/",
 } as const;
 
 export default config;
