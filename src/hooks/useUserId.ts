e55c6996e7e6d7e0e485c662c59a165f010686ec
anhsayshello <anhle0347@gmail.com>
2025-08-07 23:43:10 +0700

    chore: remove unused/commented code

--- src/hooks/useUserId.ts
+++
@@ -1,6 +0,0 @@
-import { useProfileStore } from "@/stores/useProfileStore";
-
-export const useUserId = () => {
-  const profile = useProfileStore((state) => state.profile);
-  return profile ? profile.id : "";
-};
