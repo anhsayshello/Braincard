1ebe162a2c3ded3c83a97f3631007eb76d87ffd9
anhsayshello <anhle0347@gmail.com>
2025-08-09 21:31:55 +0700

    perf: get user data state

--- src/hooks/useUserQuery.ts
+++ src/hooks/useUserQuery.ts
@@ -1,14 +1,17 @@
 import userApi from "@/apis/user.api";
 import { useAuthenticatedStore } from "@/stores/useAuthenticatedStore";
+import { useProfileStore } from "@/stores/useProfileStore";
 import { useQuery } from "@tanstack/react-query";
 
 const useUserQuery = () => {
   const isAuthenticated = useAuthenticatedStore(
     (state) => state.isAuthenticated
   );
+  const profile = useProfileStore((state) => state.profile);
+  console.log(profile, "profile store");
 
   return useQuery({
-    queryKey: ["me"],
+    queryKey: ["me", profile?.id],
     queryFn: () => userApi.getMe(),
     enabled: isAuthenticated,
     staleTime: 5 * 60 * 1000,
