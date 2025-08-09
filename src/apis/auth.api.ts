95a3190c227e1521f214aa93068d86b1430d0b76
anhsayshello <anhle0347@gmail.com>
2025-08-09 21:26:57 +0700

    feat: add Google login API call

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,7 +1,8 @@
 import AuthResponse from "@/types/auth.type";
 import http from "@/utils/http";
 
-export const URL_LOGIN = "auth/login";
+export const URL_USERNAMELOGIN = "auth/usernameLogin";
+export const URL_GOOGLELOGIN = "auth/googleLogin";
 export const URL_REGISTER = "auth/register";
 export const URL_LOGOUT = "auth/logout";
 
@@ -9,8 +10,10 @@ const authApi = {
   register: (body: { name: string; username: string; password: string }) =>
     http.post<AuthResponse>(URL_REGISTER, body),
   login: (body: { username: string; password: string }) =>
-    http.post<AuthResponse>(URL_LOGIN, body),
+    http.post<AuthResponse>(URL_USERNAMELOGIN, body),
   logout: () => http.post<string>(URL_LOGOUT),
+
+  google: (code: string) => http.post(`${URL_GOOGLELOGIN}?code=${code}`),
 };
 
 export default authApi;
