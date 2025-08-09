b50edbe0dd14cc3a268b1d42918030d98300397d
anhsayshello <anhle0347@gmail.com>
2025-08-09 21:25:53 +0700

    feat: add googleoauth provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +18,9 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <ErrorBoundary>
-          <App />
+          <GoogleOAuthProvider clientId={`${import.meta.env.VITE_CLIENT_ID}`}>
+            <App />
+          </GoogleOAuthProvider>
         </ErrorBoundary>
       </QueryClientProvider>
     </BrowserRouter>
